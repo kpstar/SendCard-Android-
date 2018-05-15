e19492872d08693047ff9c697c806a45aa239a6b
KpStar <kpstar.mc@gmail.com>
2018-05-15 20:53:44 +0800

    Last

---
+++ app/src/main/java/com/example/kpstar/sendcard/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.kpstar.sendcard;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Intent mIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mIntent);
+                SplashActivity.this.finish();
+            }
+        }, 1500);
+    }
+}
